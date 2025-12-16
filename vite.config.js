93ab5d74d41d7b5468da0d771a8ba78e33687d60
riemsis <riemsis@gmail.com>
2025-12-16 17:32:57 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/REMAURA/'
+  base: '/REMAURA/', // Pakeiskite į savo projektą, jei jis skiriasi
+  plugins: [react()]
 })
+
